be535ec14ae3042cd0d22509ac11e8b9712d344c
Thiago Serrão B. Pinto <>
2021-05-03 22:37:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+direction = input(
+    "Choose wisely where to go. Type right or Left to choose a way!").lower()
+
+
+if direction == 'left':
+    print(f"You choose {direction}, and now must choose.")
+    action = input("Do you want to swim or wait?").lower()
+    if action == "wait":
+        print(
+            f"Nice, after {action}, tree magic doors appears in front of you."
+        )
+        door = input("What door do you choose? Red, Blue or Yellow?").lower()
+        if door == "red":
+            print("Burned by magic fire. GAME OVER")
+        elif door == "blue":
+            print("Eaten by beasts. GAME OVER")
+        elif door == "yellow":
+            print("Congratulations, you did it! Well done Hero")
+        else:
+            print("You can't just write random colors... GAME OVER")
+
+    else:
+        print(
+            f"After {action}, a magical swarm of flying fish attacked you. GAME OVER"
+        )
+
+else:
+    print(f"You choose right and fall into a hole. GAME OVER")
